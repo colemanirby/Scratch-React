8456f5aecb0e78cf47b3891096fa1afac17a41bf
colemanirby <ckirbyee92@gmail.com>
2018-03-12 12:14:44 -0400

    First Commit for Vishnu

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+/*
+    ./webpack.config.js
+*/
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const BUILD_DIR = path.resolve(__dirname, './dist');
+const APP_DIR = path.resolve(__dirname, './src');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, './public/index.html'),
+});
+
+console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!! APP_DIR: " + APP_DIR);
+console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!! BUILD_DIR: " + BUILD_DIR);
+module.exports = {
+    entry: [
+        'babel-polyfill',
+        APP_DIR+ '/index.js',
+        'webpack-dev-server/client?http://localhost:8080'
+    ],
+    output: {
+        publicPath : '/',
+        filename: 'bundle.js'
+    },
+    devtool: "source-map",
+    devServer: {
+        port:8080,
+        serverSideRender:true,
+        contentBase : '/' ,
+        quiet: false,
+        stats: { colors: true },
+        proxy: {
+            "/": {
+                "target": {
+                    "host": "index.js",
+                    "protocol": 'http:',
+                    "port": 8080
+                },
+                ignorePath: true,
+                changeOrigin: true,
+                secure: false
+            }
+        }
+
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    plugins: [HtmlWebpackPluginConfig]
+};
